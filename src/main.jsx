074e76ca134c97b49bdefff9631260162086b151
Umesh Rajbanshi <ums.rbc07@gmail.com>
2026-02-22 11:00:05 -0500

    fix: use HashRouter for robust deployment on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/the-horror-portfolio">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
